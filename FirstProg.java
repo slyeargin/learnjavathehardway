ed2747d91fe069b4927b84d3fa8d9f40b4d11611
Samantha Yeargin <syeargin@changehealthcare.com>
2016-08-25 15:51:28 -0500

    Exercise 1: An Important Message.

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "I, Samantha, am determined to learn how to write Java." );
+    System.out.println( "Today's date is 9/7/2016." );
+  }
+}
